fa44536d390d804a91e1bdabe19d21bfb4fc8aee
Steven Murray <steven.murray@curtin.edu.au>
2022-01-04 11:21:03 -0700

    test: simulate_qant

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -2,7 +2,7 @@ import numpy as np
 from pathlib import Path
 
 from edges_cal import CalibrationObservation
-from edges_cal.simulate import simulate_q_from_calobs
+from edges_cal.simulate import simulate_q_from_calobs, simulate_qant_from_calobs
 
 
 def test_simulate_q(cal_data: Path):
@@ -18,3 +18,13 @@ def test_simulate_q(cal_data: Path):
 
     q2 = simulate_q_from_calobs(obsc, "open")
     np.testing.assert_allclose(q, q2)
+
+
+def test_simulate_qant(cal_data: Path):
+    obs = CalibrationObservation(cal_data)
+    q = simulate_qant_from_calobs(
+        obs,
+        ant_s11=np.zeros(obs.freq.n),
+        ant_temp=np.linspace(1, 100, obs.freq.n) ** -2.5,
+    )
+    assert len(q) == obs.freq.n
