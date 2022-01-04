36f3a10900c730d7af13c51b7f0732fb5529cb87
Steven Murray <steven.murray@curtin.edu.au>
2022-01-04 08:56:21 -0700

    test: actually use hot_load branch in simulate_q

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -9,7 +9,7 @@ def test_simulate_q(cal_data: Path):
     obs = CalibrationObservation(cal_data)
 
     q = simulate_q_from_calobs(obs, "open")
-    qhot = simulate_q_from_calobs(obs, "hot_load")
+    qhot = simulate_q_from_calobs(obs, "hot_load", freq=obs.freq.freq)
 
     assert len(q) == obs.freq.n == len(qhot)
     assert not np.all(q == qhot)
