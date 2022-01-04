7b5cd9327a0b934e8785f8b393bad61bc7220996
Steven Murray <steven.murray@curtin.edu.au>
2022-01-04 08:49:08 -0700

    test: all ways simulate_q can be called

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -1,3 +1,4 @@
+import numpy as np
 from pathlib import Path
 
 from edges_cal import CalibrationObservation
@@ -8,4 +9,12 @@ def test_simulate_q(cal_data: Path):
     obs = CalibrationObservation(cal_data)
 
     q = simulate_q_from_calobs(obs, "open")
-    assert len(q) == obs.freq.n
+    qhot = simulate_q_from_calobs(obs, "hot_load")
+
+    assert len(q) == obs.freq.n == len(qhot)
+    assert not np.all(q == qhot)
+
+    obsc = obs.to_calfile()
+
+    q2 = simulate_q_from_calobs(obsc, "open")
+    np.testing.assert_allclose(q, q2)
