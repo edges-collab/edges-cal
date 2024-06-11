fa74fd3b7735ee482a8620653f7078c13c6ccb01
Steven Murray <steven.g.murray@asu.edu>
2024-06-11 16:56:27 +0200

    fix: can't update S11Model

--- src/edges_cal/calobs.py
+++ src/edges_cal/calobs.py
@@ -1403,12 +1403,14 @@ class CalibrationObservation:
         f = new.freq.freq.to_value("MHz")
         if lna_s11 is not None:
             new._injected_lna_s11 = lna_s11
-            new.receiver.s11_model = ComplexSpline(f, lna_s11)
+            new.receiver = SimpleNamespace(s11_model=ComplexSpline(f, lna_s11))
 
         if source_s11s is not None:
             new._injected_source_s11s = source_s11s
             for name, s in source_s11s.items():
-                new.loads[name].reflections.s11_model = ComplexSpline(f, s)
+                new.loads[name].reflections = SimpleNamespace(
+                    s11_model=ComplexSpline(f, s)
+                )
 
         new._injected_c1 = c1
         new._injected_c2 = c2
