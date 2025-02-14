dcb72913a0d1a0c5885589b73bec5d571fc5e303
akvydula <akshatha.vydula@gmail.com>
2025-02-14 12:26:44 -0700

    read function for cal and uncal data from C code

--- src/edges_cal/alanmode.py
+++ src/edges_cal/alanmode.py
@@ -757,7 +757,6 @@ def read_specal(fname):
         usecols=(1, 3, 4, 6, 8, 10, 12, 14, 16),
     )
 
-
 def read_specal_iter(fname):
     """
     Read a specal file, like the ones output by edges3(k),
@@ -776,7 +775,6 @@ def read_specal_iter(fname):
         ],
         usecols=(1, 3, 5, 7, 9, 11, 13),
     )
-
 
 def read_specal_as_calibrator(
     fname: str | Path, nfit1: int = 27, t_load: float = 300, t_load_ns: float = 1000
